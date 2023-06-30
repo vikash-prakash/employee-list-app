4df871304ff4f3a8e815573d44ca07313fb55c75
Vikash <webtechvikash@gmail.com>
2023-06-30 15:32:47 +0530

    initial commit by vikas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [employees, setEmployees] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetchEmployeeData();
+  }, []);
+
+  const fetchEmployeeData = async () => {
+    try {
+      const response = await axios.get("https://reqres.in/api/users?page=2");
+      setEmployees(response.data.data);
+    } catch (error) {
+      console.error("Error fetching employee data:", error);
+    }
+  };
+
+  const filteredEmployees = employees.filter((employee) =>
+    employee.first_name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search-box">
+        <input
+          type="text"
+          placeholder="Search by first name..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      <div className="employee-list">
+        {filteredEmployees.map((employee) => (
+          <div className="employee-item" key={employee.id}>
+            <img src={employee.avatar} alt={employee.first_name} />
+            <div>{employee.first_name}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
